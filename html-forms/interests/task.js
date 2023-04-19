b51683b1a15a3867654577057e90a9093d4bdac9
Akula <6536430@gmail.com>
2023-04-19 21:28:30 +0300

    html-forms is done

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,41 +1,29 @@
-const interestCheck = document.querySelectorAll(".interest__check");
-const interest = document.querySelectorAll(".interest")
+const interestsMain = document.querySelector(".interests_main");
+const ul = interestsMain.firstElementChild;
+const parentInterest = ul.children;
 
-// function isChecked() {
-//     for (let i = 0; i < interestCheck.length; i++) {
-//         if (interestCheck[i].checked) {
-//             console.log(interestCheck[i].closest())
-//             interestCheck[i].nextElementSibling.checked;
-//         }
-//     }
-//     if (interestCheck[0].checked) {
-//         debugger;
-//         console.log(interestCheck[0].closest(".interest__check"));
-//         interestCheck[0].closest(".interest__check").checked = true;
-//     }
-// }
+for (let i = 0; i < parentInterest.length; i++) {
+    const label = parentInterest[i].firstElementChild;
+    const parentInterestCheck = label.firstElementChild;
+    const childCheck = parentInterest[i].lastElementChild;
 
-// function isChecked() {
-//     if (interestCheck[0].checked === true) {
-//         console.log(interestCheck[0].parentElement.nextElementSibling);
-//     }
-// }
+    function isChecked() {
+        if (parentInterestCheck.checked) {
+            const interestChildLi = childCheck.children;
+            for (let i = 0; i < interestChildLi.length; i++) {
+                const interestChild = interestChildLi[i].firstElementChild;
+                const childInput = interestChild.firstElementChild;
+                childInput.checked = true;
+            }
+        } else {
+            const interestChildLi = childCheck.children;
+            for (let i = 0; i < interestChildLi.length; i++) {
+                const interestChild = interestChildLi[i].firstElementChild;
+                const childInput = interestChild.firstElementChild;
+                childInput.checked = false;
+            }
+        }
+    }
 
-/*
- и так, я совершенно не понимаю как реализовать логику данного задания
- самый грубый метод, зная номера элементов задать их просто цифрами
- то есть interestCheck[1] и interestCheck[2] становятся checked когда interestCheck[0].checked === true
- Но, это какой-то глупый метод и его реализовывать не хочу
- Ещё была мысль найти соседний элемент interestCheck[0] если он выбран
- и найти дочерние элементы этого соседнего элемента, но там какая-то очень большая вложенность,
- и в целом не хочу привязывать решение к количеству вложенных элементов.
-
- По вёрстке как-то странно сделано, checkbox[0] и checkbox[1 и 2] как бы соседние элементы, 
- но они ещё и вложены в другие элементы, поэтому просто через соседние их не найти
-
- В общем я не знаю как это сделать.
-*/
-
-for (let i = 0; i < interestCheck.length; i++) {
-    interestCheck[i].onclick = isChecked;
+    parentInterestCheck.onclick = isChecked;
 }
