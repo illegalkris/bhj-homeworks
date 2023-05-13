bc2e392760fc102773aba809bc270845dedd71ba
Akula <6536430@gmail.com>
2023-05-13 18:01:29 +0300

    3.2

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,4 +1,3 @@
-const signinForm = document.getElementById("signin__form");
 const signin = document.getElementById("signin");
 const form = document.getElementById("signin__form");
 
@@ -12,5 +11,5 @@ form.onsubmit = (() => {
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     let formData = new FormData(form);
     xhr.send(formData);
-    console.log(xhr.response);
+    console.log(formData);
 })
