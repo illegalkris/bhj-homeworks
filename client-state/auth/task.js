891ff0716c3206650c7ec012b0531a337f7ca938
Akula <6536430@gmail.com>
2023-05-23 07:48:41 +0300

    auth is done

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,5 +1,8 @@
 const signin = document.getElementById("signin");
 const form = document.getElementById("signin__form");
+const welcome = document.getElementById("welcome");
+const userId = document.getElementById("user_id");
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -11,5 +14,25 @@ form.onsubmit = (() => {
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     let formData = new FormData(form);
     xhr.send(formData);
-    console.log(formData);
+
+    xhr.addEventListener("readystatechange", () => {
+        if(xhr.readyState === xhr.DONE) {
+            if (xhr.response.success) {
+                localStorage.setItem('user_id', xhr.response.user_id);
+                userId.innerHTML = `${xhr.response.user_id}`;
+                signin.classList.remove("signin_active");
+                welcome.classList.add("welcome_active");
+            } else {
+                alert("Неверный логин/пароль");
+            }
+        } 
+    })
 })
+
+window.onload = () => {
+    if(localStorage.user_id) {
+        userId.innerHTML = `${localStorage.user_id}`;
+        signin.classList.remove("signin_active");
+        welcome.classList.add("welcome_active");
+    }
+}
